0c5f7952b147e342b0b6cfcf6a673c7f4910f178
Balieiropk1 <102447068+Balieiropk1@users.noreply.github.com>
2023-01-05 17:41:39 -0300

    Update ubuinst.sh

--- ubuinst.sh
+++ ubuinst.sh
@@ -336,7 +336,7 @@ echo ""
 echo -e "Painel WEB A.I" | figlet
 echo -e "                              \033[1;31mBy @PainelWebAI\033[1;36m"
 echo ""
-chave=$(curl -sSL "raw.githubusercontent.com/Balieiropk1/pweb/painel/install/chave") &>/dev/null
+chave=$(curl -sSL "https://raw.githubusercontent.com/Balieiropk1/pweb4/main/install/chave") &>/dev/null
 
 read -p "DIGITE A CHAVE DE INSTALAÇÃO: " key
     
